21b6db29cd3212bfff8a08d39c072547ea4972ea
Abhishek Bhonde <abhishek.bhonde@refactorq.com>
2025-10-05 17:12:31 +0530

    feat: changes in the css

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import { useControls } from "leva";
